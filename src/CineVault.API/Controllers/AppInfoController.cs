5614d794b22c81a01edffa9072e9c9af449f7e19
o.razdorin <oleksii.razdorin@gmail.com>
2025-01-12 18:28:02 +0200

    AppInfoController with an /api/environment endpoint that returns the current environment

---
+++ src/CineVault.API/Controllers/AppInfoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CineVault.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppInfoController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+
+        // Інжекція залежностей для отримання середовища
+        public AppInfoController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        // Ендпоінт для отримання поточного середовища
+        [HttpGet("environment")]
+        public IActionResult GetEnvironment()
+        {
+            return Ok(new { Environment = _env.EnvironmentName });
+        }
+    }
+}
