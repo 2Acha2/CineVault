918e73cc6eb61db7c111ad768d1e870583429ed9
o.razdorin <oleksii.razdorin@gmail.com>
2025-01-18 12:34:26 +0200

    Modified Program.cs for versioning

--- src/CineVault.API/Program.cs
+++ src/CineVault.API/Program.cs
@@ -1,31 +1,72 @@
+using Asp.Versioning;
 using CineVault.API.Extensions;
 using Microsoft.AspNetCore.Mvc;
+
 [assembly: ApiController]
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                      .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+Console.WriteLine($"Active Environment: {builder.Environment.EnvironmentName}");
 
 builder.Services.AddCineVaultDbContext(builder.Configuration);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "CineVault API",
+        Version = "v1"
+    });
+    options.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "CineVault API",
+        Version = "v2"
+    });
+});
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));
+})
+.AddMvc() // This is needed for controllers
+.AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'V";
+    options.SubstituteApiVersionInUrl = true;
+});
+
 
 var app = builder.Build();
 
-Console.WriteLine($" Active Environment: {app.Environment.EnvironmentName}");
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "CineVault API v1");
+        options.SwaggerEndpoint("/swagger/v2/swagger.json", "CineVault API v2");
+    });
+}
+if (app.Environment.IsEnvironment("Local"))
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 if (app.Environment.IsLocal())
 {
     app.UseDeveloperExceptionPage();
-}
-
-if (app.Environment.IsDevelopment() || app.Environment.EnvironmentName == "Local")
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
