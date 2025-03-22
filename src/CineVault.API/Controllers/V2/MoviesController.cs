f302973ee62a7de39f1692724845872a5c73c25c
o.razdorin <oleksii.razdorin@gmail.com>
2025-03-23 00:37:02 +0200

    Task 3 movie search

--- src/CineVault.API/Controllers/V2/MoviesController.cs
+++ src/CineVault.API/Controllers/V2/MoviesController.cs
@@ -46,6 +46,68 @@ public sealed class MoviesController : ControllerBase
         return Ok(ApiResponseDto<MovieDto>.Success(movie.Adapt<MovieDto>(), "Movie retrieved successfully"));
     }
 
+    [HttpGet]
+    public async Task<ActionResult<ApiResponseDto<List<MovieDto>>>> SearchMovies(
+    string? title = null,
+    string? genre = null,
+    string? director = null,
+    int? releaseYear = null,
+    double? averageRating = null,
+    string? orderBy = "releaseDate_desc",
+    int page = 1,
+    int pageSize = 10)
+    {
+        var query = _dbContext.Movies
+            .Include(m => m.Reviews)
+            .AsQueryable();
+
+        if (!string.IsNullOrEmpty(title))
+        {
+            query = query.Where(m => m.Title.Contains(title));
+        }
+        if (!string.IsNullOrEmpty(genre))
+        {
+            query = query.Where(m => m.Genre.Contains(genre));
+        }
+        if (!string.IsNullOrEmpty(director))
+        {
+            query = query.Where(m => m.Director.Contains(director));
+        }
+
+
+        if (releaseYear.HasValue)
+        {
+            query = query.Where(m => m.ReleaseDate.HasValue && m.ReleaseDate.Value.Year == releaseYear.Value);
+        }
+
+
+        if (averageRating.HasValue)
+        {
+            query = query.Where(m => m.Reviews.Any() && m.Reviews.Average(r => r.Rating) >= averageRating.Value);
+        }
+
+       
+        query = orderBy switch
+        {
+            "title" => query.OrderBy(m => m.Title),
+            "title_desc" => query.OrderByDescending(m => m.Title),
+            "releaseDate" => query.OrderBy(m => m.ReleaseDate),
+            "rating" => query.OrderBy(m => m.Reviews.Average(r => r.Rating)),
+            "rating_desc" => query.OrderByDescending(m => m.Reviews.Average(r => r.Rating)),
+            _ => query.OrderByDescending(m => m.ReleaseDate) // releaseDate_desc за замовчуванням
+        };
+
+    
+        var totalMovies = await query.CountAsync();
+        var movies = await query
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ProjectToType<MovieDto>()
+            .ToListAsync();
+
+        return Ok(ApiResponseDto<List<MovieDto>>.Success(movies, $"Found {totalMovies} movies"));
+    }
+
     [HttpPost]
     public async Task<ActionResult<ApiResponseDto<MovieDto>>> CreateMovie([FromBody] ApiRequestDto<MovieDto> request)
     {
